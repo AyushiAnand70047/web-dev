aeb2bd6eb537767689393e20610dc755af138da2
AyushiAnand70047 <ayushianand088@gmail.com>
2025-04-05 11:53:09 +0530

    feat: initialize fullstack SQL project with Prisma and Express
    - Added package.json with necessary dependencies and scripts.
    - Created initial Prisma migration for User table with relevant fields and constraints.
    - Added migration lock file for version control.
    - Defined Prisma schema for User model with appropriate data types and defaults.
    - Implemented authentication route for user registration.

---
+++ Javascript/fullstack sql/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+// custom routes
+import userRouter from "./routes/auth.routes.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4000
+
+app.use(cookieParser)
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/",(req,res) => {
+    res.status(200).json({
+        success: true,
+        message: "test checked"
+    })
+})
+
+app.use('/api/v1/users',userRouter)
+
+app.listen(port,()=>{
+    console.log(`backend is listening at ${port}`)
+})
